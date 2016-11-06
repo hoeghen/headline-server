bd77729acd0328e5bc23b3043e32bccb3e2a55c3
hoeghen <carverdk@yahoo.dk>
2016-11-06 09:57:26 +0100

    working unit test with promise Feedly

--- js/Feedly.js
+++ js/Feedly.js
@@ -5,169 +5,150 @@
  */
 
 var NodeRequester = require("xmlhttprequest").XMLHttpRequest;
+var urlCache = [];
+
+
 var Feedly = {
     maxAge: 1000 * 60 * 5,
     maxNews: 100,
     maxProviders:10,
-  
-    request: function (url, callback) {
-        xmlhttp = new NodeRequester();
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4){
-                if(xmlhttp.status = 200){
-                    callback(xmlhttp.responseText);
-                }else{
-                    console.log("failed request " + url)
-                }
-            }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-    },
 
-    getProviders: function (callback) {
-
-        this.request('https://cloud.feedly.com/v3/search/feeds?query=nyheder&locale=da_DK&count='+this.maxProviders, function (body) {
-            callback(parse(body))
-        });
-
-        function parse(body) {
-            var providers = [];
-            var response = JSON.parse(body);
-            var results = response.results;
-            results.forEach(function (result) {
-                var provider = {};
-                provider.feedId = result.feedId;
-                provider.name = result.website;
-                if (provider.name == null) {
-                    provider.name = result.title
-                }
-                provider.iconUrl = result.iconUrl;
-                providers.push(provider)
-            });
-            return providers
-        }
-    },
-
-    parseEntries: function (provider, result) {
-        var self=this;
-        function stripHtml(html){
-            return html.replace(/<(?:.|\n)*?>/gm, '');
-        }
-        var parsed = JSON.parse(result);
-        var entries = [];
-        if (parsed.items) {
-            parsed.items.forEach(function (item) {
-                var newsItem = {};
-                newsItem.title = item.title;
-                if (item.summary){
-                    var wrapped = "<![CDATA["+item.summary.content+"]]>";
-                    var strippedContent = stripHtml(item.summary.content);
-                    newsItem.summary = strippedContent
-                }
-                newsItem.content = item.content;
-                if (item.visual && item.visual.url){
-                    newsItem.visual = item.visual.url;
-                }
-
-
-                newsItem.published = new Date(item.published).toDateString();
-                newsItem.timeStamp = item.published;
-                newsItem.provider = provider.name;
-                if (newsItem.visual  && newsItem.visual.length > 10) {
-                    if(newsItem.summary && newsItem.summary.length > 10){
-                        if(self.urlCache.indexOf(newsItem.visual)==-1){
-                            entries.push(newsItem)
-                            self.urlCache.push(newsItem.visual)
-                        }
-                    }
-                }
-            });
-        }
-        return entries
-    },
-
-    getProviderNews: function (provider, newerThan, callback) {
-        var self = this
-        var feedid = provider.feedId;
-        if (newerThan != null) {
-            this.request('https://cloud.feedly.com/v3/streams/contents?streamId=' + feedid + '&count='+this.maxNews+'&newerThan=' + newerThan, handleResult)
-        } else {
-            this.request('https://cloud.feedly.com/v3/streams/contents?streamId=' + feedid + '&count='+this.maxNews, handleResult)
-        }
-
-        function handleResult(result) {
-            var entries = self.parseEntries(provider, result);
-            callback(entries)
-        }
-
-    },
-
-    getSomeNews: function (antal,callBack) {
-        var self = this
-        var items
-        // only call for new news every maxAge minutes or first time
-
-        function isToOld() {
-            return self.feedcache.lastUpdate != null && Date.now() - self.feedcache.lastUpdate > this.maxAge;
-        }
-
-        function firstTime() {
-            if(!self.feedcache.lastUpdate){
-                return true
-            }else{
-                return false
-            }
-        }
-
-        if (true ||firstTime() ||  isToOld() ) {
-            this.getAllNewsFrom(null, function (list) {
+    getSomeNews: function (antal) {
+        return new Promise(function(resolve,reject){
+            getAllNewsFrom(null, function (list) {
                 list.sort(function(a,b){
                     return b.timeStamp - a.timeStamp;
                 })
-                self.feedcache.lastUpdate = Date.now
-                self.feedcache.data = list;
                 if(antal){
-                    callBack(list.slice(0,antal));
+                    resolve(list.slice(0,antal));
                 }else{
-                    callBack(list);
+                    resolve(list);
                 }
-
             })
-        } else {
-            callBack(self.feedcache.data)
-        }
-
-
-    },
-    getAllNews: function(callback){
-        var self = this
-      return self.getSomeNews(100,callback);
-    },
-    getAllNewsFrom: function (newerThan, callback) {
-        var self = this
-        this.getProviders(function (result) {
-            var allList = [];
-            var providers = result
-            var count = providers.length;
-
-            providers.forEach(function (provider) {
-                self.getProviderNews(provider, newerThan, function (list) {
-                    count = count - 1;
-                    if (list.length > 0) {
-                        allList.push.apply(allList, list);
-                    }
-                    if (count == 0) {
-                        callback(allList)
-                    }
-                })
-            })
-
         })
 
+
+    }
+
+}
+
+function getProviderNews(provider, newerThan, callback) {
+    var self = this
+    var feedid = provider.feedId;
+    if (newerThan != null) {
+        request('https://cloud.feedly.com/v3/streams/contents?streamId=' + '/streams/contents?streamId=' + feedid + '&count='+this.maxNews+'&newerThan=' + newerThan, handleResult)
+    } else {
+        request('https://cloud.feedly.com/v3/streams/contents?streamId=' + feedid + '&count='+this.maxNews, handleResult)
+    }
+
+    function handleResult(result) {
+        var entries = parseEntries(provider, result);
+        callback(entries)
+    }
+
+}
+
+
+
+function getAllNewsFrom(newerThan, callback) {
+    getProviders(function (result) {
+        var allList = [];
+        var providers = result
+        var count = providers.length;
+
+        providers.forEach(function (provider) {
+            getProviderNews(provider, newerThan, function (list) {
+                count = count - 1;
+                if (list.length > 0) {
+                    allList.push.apply(allList, list);
+                }
+                if (count == 0) {
+                    callback(allList);
+                }
+            })
+        })
+
+    })
+
+}
+
+
+function request(url, callback) {
+    xmlhttp = new NodeRequester();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4){
+            if(xmlhttp.status = 200){
+                callback(xmlhttp.responseText);
+            }else{
+                console.log("failed request " + url)
+            }
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function getProviders(callback) {
+
+    request('https://cloud.feedly.com/v3/search/feeds?query=nyheder&locale=da_DK&count='+this.maxProviders, function (body) {
+        callback(parse(body))
+    });
+
+    function parse(body) {
+        var providers = [];
+        var response = JSON.parse(body);
+        var results = response.results;
+        results.forEach(function (result) {
+            var provider = {};
+            provider.feedId = result.feedId;
+            provider.name = result.website;
+            if (provider.name == null) {
+                provider.name = result.title
+            }
+            provider.iconUrl = result.iconUrl;
+            providers.push(provider)
+        });
+        return providers
     }
 }
 
+function parseEntries(provider, result) {
+    function stripHtml(html){
+        return html.replace(/<(?:.|\n)*?>/gm, '');
+    }
+
+    var parsed = JSON.parse(result);
+    var entries = [];
+    if (parsed.items) {
+        parsed.items.forEach(function (item) {
+            var newsItem = {};
+            newsItem.title = item.title;
+            if (item.summary){
+                var wrapped = "<![CDATA["+item.summary.content+"]]>";
+                var strippedContent = stripHtml(item.summary.content);
+                newsItem.summary = strippedContent
+            }
+            newsItem.content = item.content;
+            if (item.visual && item.visual.url){
+                newsItem.visual = item.visual.url;
+            }
+
+
+            newsItem.published = new Date(item.published).toDateString();
+            newsItem.timeStamp = item.published;
+            newsItem.provider = provider.name;
+            if (newsItem.visual  && newsItem.visual.length > 10) {
+                if(newsItem.summary && newsItem.summary.length > 10){
+                    if(urlCache.indexOf(newsItem.visual)==-1){
+                        entries.push(newsItem)
+                        urlCache.push(newsItem.visual)
+                    }
+                }
+            }
+        });
+    }
+    return entries
+}
 
 
 
--- test/feedly-test.js
+++ test/feedly-test.js
@@ -5,45 +5,16 @@ var chaisorted = require('chai-sorted')
 chai.use(chaisorted)
 
 describe('feedly', function() {
-    describe('getProviders', function () {
-        it('should return a list of providers', function (done) {
-
-            feedly.getProviders(function(response){
-                var providers = response
-                chai.expect(providers).to.be.a('array');
-                providers.forEach(function(item){
-                    chai.expect(item).to.include.keys('feedId','name')
-                })
-                done();
-            });
-
-        });
-    });
-    describe('getProviderNews', function () {
-        this.timeout(5000);
-        it('should return a list of news', function (done1) {
-            feedly.getProviders(function(response){
-                var providers = response
-                var provider = providers[0]
-                feedly.getProviderNews(provider,null,function(providerNews){
-                    var providerNewsList = providerNews
-                    chai.expect(providerNewsList).to.be.a('array');
-                    done1()
-                })
-
-            })
-        });
-    });
-
     describe('getSomeNews', function () {
         this.timeout(5000);
         it('should return a list of some news', function (done) {
-            feedly.getSomeNews(1000,function(response){
-                chai.expect(response).to.be.a('array');
-                chai.expect(response).to.be.to.be.sortedBy('timeStamp',true)
-                done()
-            })
-        });
+            var p = feedly.getSomeNews(1000)
+            p.then(function(response){
+                    chai.expect(response).to.be.a('array');
+                    chai.expect(response).to.be.to.be.sortedBy('timeStamp',true)
+                    done()
+                }
+            )
+         });
     });
- 
 });
